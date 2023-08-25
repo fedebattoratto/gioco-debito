807892e57f3a64d9692eb43f7246a77302d87bec
fedebattoratto <samuele290506@gmail.com>
2023-08-25 16:40:25 +0200

    revisione gioco e controllo di errori

--- main.py
+++ main.py
@@ -34,10 +34,7 @@ WIZARD_SCALE = 3
 WIZARD_OFFSET = [112, 107]
 WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
 
-
 bg_image = pygame.image.load("background.jpg").convert_alpha()
-
-
 warrior_sheet = pygame.image.load("warrior.png").convert_alpha()
 wizard_sheet = pygame.image.load("wizard.png").convert_alpha()
 
@@ -47,20 +44,22 @@ WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
 WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
 #inserimento audio 
 
-
 music=pygame.mixer.Sound("musica2.mp3")
 music.play()
 
 def draw_text(text, font, text_col, x, y):
+  
   img = font.render(text, True, text_col)
   screen.blit(img, (x, y))
 
 def draw_bg():
+
   scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
   screen.blit(scaled_bg, (0, 0))
 
 
 def draw_health_bar(health, x, y):
+
   ratio = health / 100
   pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
   pygame.draw.rect(screen, RED, (x, y, 400, 30))
@@ -73,6 +72,7 @@ fighter_2 = Fighter(2, 700, 310, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATI
 
 #game loop
 run = True
+
 while run:
 
   clock.tick(FPS)
@@ -87,9 +87,12 @@ while run:
     
     fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2, round_over)
     fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1, round_over)
+
   else:
 
+
     if (pygame.time.get_ticks() - last_count_update) >= 1000:
+
       intro_count -= 1
       last_count_update = pygame.time.get_ticks()
 
@@ -102,18 +105,25 @@ while run:
   fighter_2.draw(screen)
 
   if round_over == False:
+
     if fighter_1.alive == False:
+
       score[1] += 1
       round_over = True
       round_over_time = pygame.time.get_ticks()
+
     elif fighter_2.alive == False:
+
       score[0] += 1
       round_over = True
       round_over_time = pygame.time.get_ticks()
+
   else:
    
     screen.blit(victory_img, (360, 150))
     if pygame.time.get_ticks() - round_over_time > ROUND_OVER_COOLDOWN:
+
+
       round_over = False
       intro_count = 3
       fighter_1 = Fighter(1, 200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS)
@@ -121,10 +131,12 @@ while run:
 
 
   for event in pygame.event.get():
+
+
     if event.type == pygame.QUIT:
+
       run = False
 
   pygame.display.update()
-
 
 pygame.quit()
